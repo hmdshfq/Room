3bb354a01ddd28dbd218abdebc8ea6e46122a5e2
Hammad <hmdshfq@outlook.com>
2020-11-26 16:22:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Room
+ A simple react app that display a dark and litup room
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [isLit, setLit] = useState(true);
+  const [temp, setTemp] = useState(22);
+  return (
+    <div className={`room ${isLit ? "lit" : "dark"}`}>
+      <h1>The room is {isLit ? "lit" : "dark"}</h1>
+      <h1>The temperature of the room is {temp}</h1>
+      <div>
+        <button onClick={() => setLit(true)}>Switch ON</button>
+        <button onClick={() => setLit(false)}>Switch OFF</button>
+        <button onClick={() => setTemp(temp - 1)}>Decrease Temperature</button>
+        <button onClick={() => setTemp(temp + 1)}>Increase Temperature</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
